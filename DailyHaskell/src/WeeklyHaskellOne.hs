dfbb3738787924c04d8b2b0c266d1487deb1af1c
Chad M <chadmonteithii@gmail.com>
2022-04-22 20:57:44 -0600

    weekly one submission

--- DailyHaskell/src/WeeklyHaskellOne.hs
+++ DailyHaskell/src/WeeklyHaskellOne.hs
@@ -1,2 +1,50 @@
 module WeeklyHaskellOne where
-    
+
+--function that removes all instances of a character from a string
+--accepts a character and a string then returns a string
+removeChar :: Char -> String -> String
+removeChar c [] = []
+removeChar c (x:xs)
+    | c == x = removeChar c xs
+    | otherwise = x : removeChar c xs
+
+--function that removes the whitespace from a string
+--accepts a string then returns a string
+removeWhitespace :: String -> String
+removeWhitespace [] = []
+removeWhitespace str = removeChar ' ' (removeChar '\t' (removeChar '\r' (removeChar '\n' str)))
+
+--function that removes the punctuation from a string
+--accepts a string then returns a string
+removePunctuation :: String -> String
+removePunctuation "\&" = "\&"
+removePunctuation str = removeChar '.' (removeChar ',' (removeChar '[' (removeChar ']' 
+    (removeChar '(' (removeChar ')' (removeChar '{' (removeChar '}' str)))))))
+
+--function that converts each character in a string to its respective ASCII value
+--into an list
+--accepts a string and returns a list of integers
+charsToAscii :: String -> [Int]
+charsToAscii [] = []
+charsToAscii (x:xs) = fromEnum (x :: Char) : charsToAscii xs
+
+--function that converts a list of ASCII values to the respective character list (a string)
+--accepts a list of integers then returns a list of characters (essentially a string)
+asciiToChars :: [Int] -> [Char]
+asciiToChars [] = []
+asciiToChars (x:xs) = toEnum x : asciiToChars xs
+
+--function that shifts each value in an integer list by a given number, from 0-127
+--accepts an integer and a list of integers then returns a list of integers
+shiftInts :: Int -> [Int] -> [Int]
+shiftInts _ [] = []
+shiftInts 0 [x] = [x]
+shiftInts x (y:ys) = (x + y) `mod` 128: shiftInts x ys
+
+--function that works as a Caesar Cipher, by shifting a string by a given value
+--in the ASCII alphabet
+--accepts an integer and a string then returns a string
+shiftMessage :: Int -> String -> String
+shiftMessage 0 [x] = [x]
+shiftMessage _ [] = []
+shiftMessage x y = asciiToChars (shiftInts x (charsToAscii y))
--- DailyHaskell/test/WeeklyHaskellOneSpec.hs
+++ DailyHaskell/test/WeeklyHaskellOneSpec.hs
@@ -1,1 +1,56 @@
 module WeeklyHaskellOneSpec where
+
+import Test.Hspec
+import WeeklyHaskellOne
+
+spec :: Spec
+spec = do
+    describe "removeChar" $ do
+        it "does something" $
+            removeChar 'a' "abcde" `shouldBe` "bcde"
+        it "does something" $
+            removeChar 'y' "hello" `shouldBe` "hello"
+        it "does something" $
+            removeChar 'y' "" `shouldBe` ""
+    describe "removeWhitespace" $ do
+        it "does something" $
+            removeWhitespace " h e l l o " `shouldBe` "hello"
+        it "does something" $
+            removeWhitespace "a     b       c  " `shouldBe` "abc"
+        it "does something" $
+            removeWhitespace "this is a string" `shouldBe` "thisisastring"
+    describe "removePunctuation" $ do
+        it "does something" $
+            removePunctuation "h.e.l.l.o." `shouldBe` "hello"
+        it "does something" $
+            removePunctuation "(i {dont like} [to code])" `shouldBe` "i dont like to code"
+        it "does something" $
+            removePunctuation "" `shouldBe` ""
+    describe "charsToAscii" $ do
+        it "does something" $
+            charsToAscii "hello" `shouldBe` [104,101,108,108,111]
+        it "does something" $
+            charsToAscii "test...ing" `shouldBe` [116,101,115,116,46,46,46,105,110,103]
+        it "does something" $
+            charsToAscii "" `shouldBe` []
+    describe "asciiToChars" $ do
+        it "does something" $
+            asciiToChars [104,101,108,108,111] `shouldBe` "hello"
+        it "does something" $
+            asciiToChars [116,101,115,116,46,46,46,105,110,103] `shouldBe` "test...ing"
+        it "does something" $
+            asciiToChars [] `shouldBe` ""
+    describe "shiftInts" $ do
+        it "does something" $
+            shiftInts 1 [2,4,6,127] `shouldBe` [3,5,7,0]
+        it "does something" $
+            shiftInts 128 [1,1,1,1,1] `shouldBe` [1,1,1,1,1]
+        it "does something" $
+            shiftInts 5 [] `shouldBe` []
+    describe "shiftMessage" $ do
+        it "does something" $
+            shiftMessage 1 "hello" `shouldBe` "ifmmp"
+        it "does something" $
+            shiftMessage (-1) "ifmmp" `shouldBe` "hello"
+        it "does something" $
+            shiftMessage 10 "" `shouldBe` ""
